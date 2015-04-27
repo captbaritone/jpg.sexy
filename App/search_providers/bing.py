552ba9a434e7070945d9e68d5a4c3fb8ca335fd6
Jordan Eldredge <jordaneldredge@gmail.com>
2015-04-26 23:38:29 -0700

    Remove NSFW results from Bing

--- App/search_providers/bing.py
+++ App/search_providers/bing.py
@@ -7,6 +7,14 @@ class BingSearcher(object):
 
     @classmethod
     def images(cls, query):
+        html = cls._fetch_html(query)
+        urls = cls._url_matches(html)
+        # app.logger.debug("Bing: Found %s URLs mathing query '%s'" % (len(urls), query))
+
+        return urls
+
+    @classmethod
+    def _fetch_html(cls, query):
         payload = {
             'qft': '+filterui:photo-photo',
             'q': query
@@ -14,21 +22,17 @@ class BingSearcher(object):
         try:
             html = requests.get('http://www.bing.com/images/search', params=payload).text
         except RequestException:
+            html = ''
             # app.logger.warning('Request failed while searching Bing images for %' % query)
-            return []
-
-        urls = cls._url_matches(html)
-        # Exclude matches including 'fz="1"'
-        # app.logger.debug("Bing: Found %s URLs mathing query '%s'" % (len(urls), query))
-
-        return urls
+        return html
 
     @classmethod
     def _url_matches(cls, html):
-        img_url_matcher = re.compile('( m="{[^\"]*imgurl:&quot;([^\&]*)&quot;[^\"]*}")')
+        img_url_matcher = re.compile('(<[^<]* m="{[^\"]*imgurl:&quot;([^\&]*)&quot;[^\"]*}")')
         matches = img_url_matcher.findall(html)
 
-        return [m[1] for m in matches]
+        return [m[1] for m in matches if cls._is_sfw(m)]
 
+    @classmethod
     def _is_sfw(cls, match):
-        return True
+        return re.search('fz="1"', match[0]) is None
