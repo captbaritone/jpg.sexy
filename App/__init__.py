55e7a90f39899a14d9d494715b8fc02129a55636
Jordan Eldredge <jordaneldredge@gmail.com>
2015-08-02 14:14:36 -0700

    Simpler lookup approach

--- App/__init__.py
+++ App/__init__.py
@@ -1,42 +1,22 @@
-from flask import Flask, redirect
-from requests.exceptions import ConnectionError
-from search_providers.bing import BingSearcher
-import requests
+from flask import Flask
+
 app = Flask(__name__)
+app.config.update(SERVER_NAME='localhost:5000')
+
+lookup = {
+    'hello': 'hello.jpg',
+    'garland-dance': 'garland-dance.gif',
+}
 
 
-@app.route("/")
-def welcome():
-    return "search-query.jpg.sexy"
-
-
-@app.route("/", subdomain="<query>")
-@app.route("/<query>")
-def redirect_to_image(query):
-    app.logger.debug("Looking for an image matching '%s'" % query)
-    for url in BingSearcher.images(query):
-        if is_live_url(url):
-            app.logger.debug("Redirecting to '%s'" % url)
-            return redirect(url)
-    app.logger.debug("No images found :(")
-    return "No results"
-
-
-def is_live_url(url):
-    app.logger.debug("Testing validity of URL '%s'" % url)
+@app.route("/", subdomain='<query>')
+def subdomains(query):
     try:
-        r = requests.request('HEAD', url, allow_redirects=True)
-        is_live = r.status_code in [301, 302, 200]
-    except ConnectionError:
-        app.logger.debug("URL '%s' is dead. ConnectionError" % url)
-        return False
+        filename = lookup[query]
+    except LookupError:
+        filename = 'hello.jpg'
 
-    if is_live:
-        app.logger.debug("URL '%s' is live with status code %s" % (url, r.status_code))
-        return True
-    else:
-        app.logger.debug("URL '%s' is dead with status code %s" % (url, r.status_code))
-        return False
+    return app.send_static_file(filename)
 
 if __name__ == "__main__":
     app.run(debug=True)
